dac9fcd53eeb1cdad4f669e769a839f523d8df96
Ryan Kirsch <agalloch88@gmail.com>
2022-04-23 22:47:05 -0400

    finish styling of trailer modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from 'react'
 import MuiModal from '@mui/material/Modal'
 import { useRecoilState } from 'recoil'
 import { modalState, movieState } from '../atoms/modalAtom'
-import { PlusIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon, XIcon } from '@heroicons/react/outline'
+import {
+  PlusIcon,
+  ThumbUpIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+  XIcon,
+} from '@heroicons/react/outline'
 import { Element, Genre } from '../typings'
 import ReactPlayer from 'react-player/lazy'
 import { FaPlay } from 'react-icons/fa'
@@ -65,24 +71,60 @@ function Modal() {
             playing
             muted={muted}
           />
-          <div className='absolute bottom-10 flex w-full items-center justitfy-between px-10'>
+          <div className="justitfy-between absolute bottom-10 flex w-full items-center px-10">
             <div className="flex space-x-2">
               <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
                 <FaPlay className="h-7 w-7 text-black" />
                 Play
               </button>
-              <button className='modalButton'>
-                  <PlusIcon className='h-7 w-7'/>
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
               </button>
               <button className="modalButton">
-                  <ThumbUpIcon className='h-7 w-7'/>
+                <ThumbUpIcon className="h-7 w-7" />
               </button>
             </div>
-            <button className='modalButton' onClick={() => setMuted(!muted)}>
-                {muted ? (
-                    <VolumeOffIcon className='h-6 w-6' />
-                ) : <VolumeUpIcon className='h-6 w-6' />}
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <VolumeOffIcon className="h-6 w-6" />
+              ) : (
+                <VolumeUpIcon className="h-6 w-6" />
+              )}
             </button>
+          </div>
+        </div>
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres:</span>{' '}
+                  {genres.map((genre) => genre.name).join(', ')}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original language:</span>{' '}
+                  {movie?.original_language}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Total votes:</span>{' '}
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
