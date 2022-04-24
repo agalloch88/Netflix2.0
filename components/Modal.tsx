8552ad439e3f5fc1198a2947f2f5fad795004721
Ryan Kirsch <agalloch88@gmail.com>
2022-04-23 21:34:20 -0400

    add react-player, start on fetchMovie

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MuiModal from '@mui/material/Modal'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useRecoilState } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
 import { XIcon } from '@heroicons/react/outline'
+import { Movie } from '../typings'
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState)
+  const [movie, setMovie] = useState<Movie | null>(null)
 
   const handleClose = () => {
     setShowModal(false)
   }
+
+  useEffect(() => {
+      if (!movie) {
+          return
+      }
+
+      async function fetchMovie() {
+          const data = await fetch(`https://api.themoviedb.org/3/${
+            movie?.media_type === 'tv' ? 'tv' : 'movie'
+          }/${movie?.id}?api_key=${
+            process.env.NEXT_PUBLIC_API_KEY
+          }&language=en-US&append_to_response=videos`)
+      }
+  },[])
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
@@ -18,7 +34,7 @@ function Modal() {
           <XIcon className="h-6 w-6" />
         </button>
         <div>
-            
+
         </div>
       </>
     </MuiModal>
