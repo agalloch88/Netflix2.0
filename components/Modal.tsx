4ffe3dd660b2f270763f48d15479870edd52ef13
Ryan Kirsch <agalloch88@gmail.com>
2022-04-23 21:27:47 -0400

    style modal close icon, create modalButton tailwind class

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MuiModal from '@mui/material/Modal'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
+import { XIcon } from '@heroicons/react/outline'
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState)
@@ -13,7 +14,12 @@ function Modal() {
   return (
     <MuiModal open={showModal} onClose={handleClose}>
       <>
-      
+        <button onClick={handleClose} className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
+          <XIcon className="h-6 w-6" />
+        </button>
+        <div>
+            
+        </div>
       </>
     </MuiModal>
   )
